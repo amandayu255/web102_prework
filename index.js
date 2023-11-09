a43dc1646578ba8f7474f642cc8b65a25975995d
Amanda Yu <amandayu255@gmail.com>
2023-11-09 14:33:33 -0800

    display topGame and runnerUp game

--- index.js
+++ index.js
@@ -193,7 +193,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [topGame, runnerUp] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topGameName = document.createElement("h3");
+topGameName.innerHTML = topGame.name;
+firstGameContainer.appendChild(topGameName);
 
 // do the same for the runner up item
+const runnerUpName = document.createElement("h3");
+runnerUpName.innerHTML = runnerUp.name;
+secondGameContainer.appendChild(runnerUpName);
